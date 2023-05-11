c29f75899de19e586b66967cab967e33a0ecc692
andymonk8 <andymonk8@gmail.com>
2023-05-11 04:58:11 -0500

    Results Displays?!

--- js/site.js
+++ js/site.js
@@ -1,76 +1,82 @@
-//get starting values from the scree
+//get starting values from the screen
 //Controller Function
 
 function getValue() {
   // Hides the Results alert
   document.getElementById("alert").classList.add("invisible");
 
-  // Get the User input
   let userString = document.getElementById("userString").value;
 
   if (userString == "") {
     Swal.fire({
       icon: "error",
       backdrop: false,
-      title: "Rewind",
-      text: "Input required!",
+      title: "Sorry!",
+      text: "Must enter a word",
     });
-  } 
-  else 
-  {
-    // Reverse the user input
-    //let revString = ReverseAStringJS(userString);
-    let revString = palindrome(userString);
-    
-    let msg = '';
-    if(userString == revString) {
-      msg = 'Your Word is a Palindrome!';
-    }
-    else{
-      msg = "Your Word is NOT a Palindrome!";
-    }
-
-
-    // Display the reversed User input
-    displayString(msg);
   }
+
+  /* Regex Gets Rid of Spaces and Special Charcters?!
+  To Lower Case / toLowerCase Makes Everything / the String Lower Case?! */
+  const regex = /[^a-z0-9]/gi;
+  userString = userString.toLowerCase().replace(regex, "");
+
+  let resultString = reverseString(userString);
+  let isPalindrome = palChecker(userString, resultString);
+
+
+  displayData(resultString, isPalindrome);
 }
 
-//Reverse the string
-function palindrome(userString) {
+// Reverse / Reversing the String?!
+function reverseString(userString) {
+
   let revString = [];
 
-  // User input = 'word'
+  // Loop?! String Length Total - 1 is the Last Chacter of the index?!
   for (let i = userString.length - 1; i >= 0; i--) {
+    
     revString += userString[i];
   }
 
-  const regex = /[^A-Za-z0-9]/gi;
-  userString = userString.toLowerCase().replace(regex, "");
 
   return revString;
 }
 
 
 
-/* //Reverse the string JS style
-function ReverseAStringJS(userString) {
-  // Split the string into an array
-  let usrArray = userString.split("");
+// Checks if Palindrome?!
+function palChecker(userString, resultString) {
 
-  // Reverse the array
-  let revArray = usrArray.reverse();
+    if (resultString.toLowerCase() === userString.toLowerCase()) {
+      isPalindrome = true;
+    }else{
+      isPalindrome = false;
+    }
 
-  // Reurn the array to a string
-  let revString = revArray.join("");
+    
+    return isPalindrome;
+}
 
-  return revString;
-} */
 
-//Display the string
-function displayString(revString) {
-  document.getElementById(
-    "msg"
-  ).innerHTML = `Your string reversed is: <strong>${revString}</strong>`;
+
+function displayData(resultString, isPalindrome) {
+  document.getElementById("alert").classList.add("alert-success");
+  document.getElementById("alert").classList.remove("alert-danger");
+
+  if (isPalindrome) {
+    document.getElementById("alert").classList.add("alert-success");
+    document.getElementById("alertHeader").innerHTML = "Well done! It is a Palindrome!";
+    document.getElementById(
+      "msg"
+    ).innerHTML = `Your reversed string is: <br><h5><strong>${resultString}<strong><h5>`;
+  } else {
+    document.getElementById("alert").classList.add("alert-danger");
+    document.getElementById("alertHeader").innerHTML = "Sorry! Not a Palindrome!";
+    document.getElementById(
+      "msg"
+    ).innerHTML = `Your reversed string is: <br><h5><strong>${resultString}<strong><h5>`;
+  }
+
   document.getElementById("alert").classList.remove("invisible");
 }
